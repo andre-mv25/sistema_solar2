3e06adce79cb39b5397f7b0984600c4b631861a6
andre miranda <lopezvargasandre@outlook.com>
2026-06-11 13:57:56 -0600

    Fix: servidor HTTP nativo sin Express

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname)));
+const MIME = {
+  '.html':'text/html','.css':'text/css','.js':'application/javascript',
+  '.json':'application/json','.png':'image/png','.jpg':'image/jpeg',
+  '.svg':'image/svg+xml','.ico':'image/x-icon'
+};
 
-app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => console.log('Servidor en puerto ' + PORT));
+http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'index.html'), (_, data404) => {
+        res.writeHead(404, {'Content-Type':'text/html'});
+        res.end(data404 || '404');
+      });
+      return;
+    }
+    res.writeHead(200, {'Content-Type': MIME[ext] || 'text/plain'});
+    res.end(data);
+  });
+}).listen(PORT, () => console.log('Servidor en puerto ' + PORT));
